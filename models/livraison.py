e0a0df57643723e10d40d7579f9bcca8ea42f9b0
Lewafal_Ghislain <fo2sghis@gmail.com>
2023-12-22 04:39:18 +0100

    premier commit avec juste le fichier de model Livraison créé

---
+++ models/livraison.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class Livraison(models.Model):
+    _name = "Gonagoo.livraison"
+    _description="Livraison"
+
+    livraison_number = id()
+
+
